6a38b27dd4eab3ef8653db3f43322aba85ba8127
alexandrecarloss <107065086+alexandrecarloss@users.noreply.github.com>
2024-03-30 22:45:01 -0400

    Login e logout

--- artemis/helloworld/accounts/urls.py
+++ artemis/helloworld/accounts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('cadastropessoa/', views.cadastropessoa, name='accounts/cadastropessoa'),
     path('cadastro/', views.cadastro, name='accounts/cadastro'),
+    path('logout/', views.logoutaccount, name='logoutaccount'),
+    path('login/', views.loginaccount, name='loginaccount'),
 ]
--- artemis/helloworld/accounts/views.py
+++ artemis/helloworld/accounts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import UserCreateForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
 
@@ -37,4 +37,19 @@ def cadastro(request):
                 return render(request, 'cadastro2.html', {'form': UserCreateForm, 'error': 'Usuário já existe. Escolha um novo usuário.'})
         else:
             return render(request, 'cadastro2.html', {'form': UserCreateForm, 'error': 'Senhas não coincidem'})
+        
+def logoutaccount(request):
+    logout(request)
+    return redirect('index')
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', {'form': AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'loginaccount.html', {'form': AuthenticationForm(), 'error': 'Usuário e senha não coincidem'})
+        else:
+            login(request, user)
+            return redirect('index')
 
